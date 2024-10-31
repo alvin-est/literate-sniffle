8086d2d984ea3e0b9866aca706c90593f52fa21d
alvin-est <estado.alvin@gmail.com>
2024-10-31 16:25:46 +1100

    added config, routes & updated server.js

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432, 
       dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      dialectOptions: { decimalNumbers: true },
+  }
+);
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync sequelize models to the database 
+(async ()  => {
+  try {
+    await sequelize.sync({ force: false }); // Set force to false in production
+  }
+  catch (err) {
+    console.error('Error syncing DB:', err);
+    process.exit(1); // Exit process on sync failure
+  }
+})();
+
+// then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
